ffb4db65a2a03a49d8b0b06aeaccb1ccbb662e85
Анастасия Сечко <asanastasia1@gmail.com>
2023-07-10 00:21:11 +0300

    12HW_Last commit

--- SelenStart/src/test/java/seleniumTests/LinksTest.java
+++ SelenStart/src/test/java/seleniumTests/LinksTest.java
@@ -12,30 +12,41 @@ import java.time.Duration;
 
 public class LinksTest {
 
-    @Test
+    @Test(description = "открыть сайт")
     public static void firstTest() throws InterruptedException {
         WebDriver driver = new ChromeDriver(); //экземпляр web-драйвера
-        String searchText = "Rubber Ducks";
-        String searchText2 = "Subcategory";
+        String searchText = "Most Popular";
 
         driver.get("https://litecart.stqa.ru/en/");//открыть ссылку
-        //WebElement links = driver.findElement(By.id("content"));
-        //WebElement link = links.findElement(By.linkText("Drag and Drop"));
-        //link.click();
-        driver.manage().window().maximize();
+        //driver.manage().window().maximize();
+        WebElement element = driver.findElement(By.xpath("//div[@id='box-most-popular']/*"));
+        Assert.assertEquals(element.getText(), searchText);
+        driver.quit();
+    }
+
+    @Test(description = "Пройти по ссылке Rubber Ducks")
+    public static void linkFirst() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        String searchText = "Rubber Ducks";
 
         driver.get("https://litecart.stqa.ru/en/rubber-ducks-c-1/");
         Thread.sleep(2000);
         WebElement element = driver.findElement(By.xpath("//*[@id='box-category']/*[@class='title']"));
         Assert.assertEquals(element.getText(), searchText);
+        driver.quit();
 
+    }
 
-        driver.get("https://litecart.stqa.ru/en/rubber-ducks-c-1/subcategory-c-2/");
-        Thread.sleep(2000);
-        WebElement element2 = driver.findElement(By.xpath("//*[@id='box-category']//*[@class='title']"));
-        Assert.assertEquals(element2.getText(), searchText2);
+    @Test(description = "переход по ссылке Subcategory")
+    public static void linkSecond() throws InterruptedException {
+    WebDriver driver = new ChromeDriver();
+    String searchText2 = "Subcategory";
 
-        driver.quit();//закрыть браузер
+    driver.get("https://litecart.stqa.ru/en/rubber-ducks-c-1/subcategory-c-2/");
+    Thread.sleep(2000);
+    WebElement element2 = driver.findElement(By.xpath("//*[@id='box-category']//*[@class='title']"));
+    Assert.assertEquals(element2.getText(), searchText2);
+    driver.quit();//закрыть браузер
     }
 
     @Test(description = "Переход по синей уточке")
@@ -47,8 +58,8 @@ public class LinksTest {
         Thread.sleep(2000);
         driver.get("https://litecart.stqa.ru/en/rubber-ducks-c-1/");
         Thread.sleep(2000);
-        //driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(50));
-        //driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(50));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(50));
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(50));
         //driver.manage().window().maximize();
         WebElement blueDuck = driver.findElement(By.xpath("//li[@class='product column shadow hover-light']/*[@href='https://litecart.stqa.ru/en/rubber-ducks-c-1/blue-duck-p-4']"));//находим нужный элемент
         Actions click = new Actions(driver);//создаём экземпляр класса
@@ -60,7 +71,7 @@ public class LinksTest {
         driver.quit();
     }
 
-    @Test(description = "Перетягивание уточки")
+    @Test(description = "Перетягивание уточки negative case")
     public static void dragAndDrop() throws InterruptedException {
         String linkInEmailField = "http://www.canstockphoto.com/?r=282295";
 
@@ -68,14 +79,90 @@ public class LinksTest {
         driver.get("https://litecart.stqa.ru/en/");
 
         WebElement duck = driver.findElement(By.xpath("//a[@href='http://www.canstockphoto.com/?r=282295']"));
-        WebElement searchField = driver.findElement(By.xpath("//*[@name='email']"));
+        WebElement searchField = driver.findElement(By.xpath("//*[@name='query']"));
         Thread.sleep(2000);
-        Actions actions = new Actions(driver);
+        Actions builder = new Actions(driver);
 
-        actions.dragAndDrop(duck,searchField).perform();
-        //Thread.sleep(2000);
+        builder.dragAndDrop(duck,searchField).perform();
+        Thread.sleep(2000);
 
-        //Assert.assertEquals(searchField.getText(),linkInEmailField);
+        Assert.assertEquals(searchField.getCssValue("writing-mode"),linkInEmailField);
+
+        //driver.quit();
+    }
+
+    @Test(description = "Перетягивание уточки positive case")
+    public static void dragAndDrop0() throws InterruptedException {
+        String linkInEmailField="";
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://litecart.stqa.ru/en/");
+
+        WebElement duck = driver.findElement(By.xpath("//li[@class='rslides1_on']/*/*"));
+        WebElement searchField = driver.findElement(By.xpath("//*[@name='query']"));
+        Thread.sleep(2000);
+        Actions builder = new Actions(driver);
+
+        builder.dragAndDrop(duck,searchField).perform();
+        Thread.sleep(2000);
+
+        Assert.assertEquals(searchField.getAttribute("value"),linkInEmailField);
+
+        driver.quit();
+    }
+
+    @Test(description = "Прописывание текста в поле")
+    public static void sendText() throws InterruptedException {
+        String someInput = "aaa";
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://litecart.stqa.ru/en/");
+
+        driver.findElement(By.xpath("//*[@name='query']")).sendKeys(someInput);
+        Thread.sleep(2000);
+        Actions builder = new Actions(driver);
+
+        Assert.assertEquals(someInput, someInput);
+
+        driver.quit();
+    }
+
+    @Test(description = "Перетягивание объекта ClassWork и проверка ошибок хромдрайвера")
+    public static void dragAndDrop2() throws InterruptedException {
+        String linkInTheField = "rgba(255, 192, 203, 1)";
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://learn.javascript.ru/article/mouse-drag-and-drop/ball4/");
+
+        WebElement ball = driver.findElement(By.id("ball"));
+        WebElement goal = driver.findElement(By.id("gate"));
+        Thread.sleep(2000);
+        Actions builder = new Actions(driver);
+
+        builder.dragAndDrop(ball,goal).perform();
+        Thread.sleep(2000);
+
+        Assert.assertEquals(goal.getCssValue("background-color"),linkInTheField);
+
+        driver.quit();
+    }
+
+    @Test(description = "Перетягивание объекта")
+    public static void dragAndDrop3() throws InterruptedException {
+        String linkInEmailField = "";
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://software-testing.ru/");
+
+        WebElement iconLink = driver.findElement(By.cssSelector("[href='http://vk.com/test_it']"));
+        WebElement field = driver.findElement(By.name("q"));
+        Thread.sleep(2000);
+        Actions builder = new Actions(driver);
+
+        builder.dragAndDrop(iconLink,field).perform();
+        Thread.sleep(2000);
+
+        Assert.assertEquals(field.getAttribute("value"), linkInEmailField);
 
         driver.quit();
     }
